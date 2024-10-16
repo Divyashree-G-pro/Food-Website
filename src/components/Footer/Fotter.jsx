9eb2683581af8179118ac3a0a9e4c93c80c72eae
Divyashree-G-pro <gdiv1810@gmail.com>
2024-10-16 15:56:31 +0530

    Initial commit

---
+++ src/components/Footer/Fotter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Fotter = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+        <div className="footer-content-left">
+                <img src={assets.logo} alt=""/>
+               
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima architecto vel facere quibusdam dolorum ut ipsam quos soluta quas quis. Pariatur?</p>
+               <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+               </div>
+               
+                </div>
+
+            <div className="footer-content-center">
+                    
+            </div>
+
+            <div className="footer-content-right">
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Fotter
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>Indulge in the flavors you love by ordering your favorite meals right here! Whether you're in the mood for a cheesy pizza, a hearty burger, or something sweet and savory, we’ve got it all.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
